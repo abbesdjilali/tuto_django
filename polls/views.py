f2becc83ef13044f62dfb8ead54fbddcf39194d4
abbesdjilali <abbesdjilali@live.fr>
2021-01-17 21:26:47 +0100

    Part 4 of tuto django

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import Http404
-from .models import Question
+from django.http import Http404,HttpResponseRedirect
+from django.urls import reverse
+from .models import Question,Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-date_published')[:5]
@@ -14,7 +15,20 @@ def detail(request,question_id):
     return render(request,'polls/detail.html',{'question':question})
 
 def results(request,question_id):
-    return HttpResponse("Page résultats pour la question : %s" %question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,"polls/results.html", {'question':question})
 
 def vote(request,question_id):
-    return HttpResponse("Votez pour la question %s" %question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    #request.POST['choice'] lèvera une exception KeyError si choice n’est pas spécifié dans les données POST
+    except (KeyError,Choice.DoesNotExist):
+        return render(request,"polls/detail.html",{
+            'question':question,
+            'error_message':"Vous n'avez pas fait de choix"
+        })
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
